dba4eaaadb4deeaea1ebb286c6b3ff4f93b8d0e0
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-12 20:23:48 -0500

    added create method and strong params

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+
+  def create
+    restaurant = Restaurant.create(Restaurant_params)
+    if restaurant.valid?
+      render json: restaurant
+    else
+      render json: restaurant.errors, status: 422
+    end
+  
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :food_type, :image, :price, :phone_number, :website, :zip, :city, :street, :state, :restaurant_id, :user_id)
+  end
 end
