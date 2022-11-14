0fba11a4ff2a8c1e9cfede87b3a70c92e6d8c55c
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-14 13:26:56 -0500

    updated test to pass with mock data

--- app/javascript/components/jest-tests/RestaurantIndex.test.js
+++ app/javascript/components/jest-tests/RestaurantIndex.test.js
@@ -1,13 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import mockRestaurants from "../mockRestaurants";
 import RestaurantIndex from "../pages/RestaurantIndex";
 
 describe("<RestaurantIndex />", () => {
   it("renders without crashing", () => {
     render(
       <BrowserRouter>
-        <RestaurantIndex />
+        <RestaurantIndex restaurants={mockRestaurants} />
       </BrowserRouter>
     );
   });
