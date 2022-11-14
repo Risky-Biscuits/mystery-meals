26aa3c99b9d3a9e5f626f4ee506cfd3c90d2ffdd
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-14 14:38:10 -0500

    added card components for restaurant data

--- app/javascript/components/pages/RestaurantIndex.js
+++ app/javascript/components/pages/RestaurantIndex.js
@@ -1,35 +1,41 @@
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import React from "react";
-const RestaurantIndex = ({restaurants}) => {
+
+const RestaurantIndex = ({ restaurants }) => {
   return (
-    <>
-      <h1>Restaurant Index</h1>
+    <div className="page-container">
+      <h1>See All Restaurants</h1>
+      <div className="card-container">
       {restaurants.map((restaurant, index) => {
-          return (
-            <div key={index}>
-              <img src={restaurant.image} width="400px"/>
-              <br/>
-              {restaurant.name}
-              <br/>
-              {restaurant.food_type}
-              <br/>
-              {restaurant.price}
-              <br/>
-              {restaurant.phone_number}
-              <br/>
-              <a href={restaurant.website} >View Website</a>
-              <br/>
-              {restaurant.zip}
-              <br/>
-              {restaurant.city}
-              <br/>
-              {restaurant.street}
-              <br/>
-              {restaurant.state}
-              <br/>
-            </div>
-          );
-        })}
-    </>
+        return (
+          <div key={index}>
+            <Card elevation={24} sx={{ maxWidth: 300, border: "5px solid #AD8350" }}>
+              <CardMedia
+                component="img"
+                height="340"
+                image={restaurant.image}
+                alt="restaurant"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {restaurant.name}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Details</Button>
+                <Button size="small">Reviews</Button>
+              </CardActions>
+            </Card>
+          </div>
+        );
+      })}
+      </div>
+    </div>
   );
 };
 
