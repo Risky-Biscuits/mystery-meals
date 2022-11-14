4f760c631e30f4a25ea640bd54c770d638aa2597
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-14 13:23:18 -0500

    restaurantindex mock data set up

--- app/javascript/components/pages/RestaurantIndex.js
+++ app/javascript/components/pages/RestaurantIndex.js
@@ -1,8 +1,34 @@
 import React from "react";
-const RestaurantIndex = () => {
+const RestaurantIndex = ({restaurants}) => {
   return (
     <>
       <h1>Restaurant Index</h1>
+      {restaurants.map((restaurant, index) => {
+          return (
+            <div key={index}>
+              <img src={restaurant.image} width="400px"/>
+              <br/>
+              {restaurant.name}
+              <br/>
+              {restaurant.food_type}
+              <br/>
+              {restaurant.price}
+              <br/>
+              {restaurant.phone_number}
+              <br/>
+              <a href={restaurant.website} >View Website</a>
+              <br/>
+              {restaurant.zip}
+              <br/>
+              {restaurant.city}
+              <br/>
+              {restaurant.street}
+              <br/>
+              {restaurant.state}
+              <br/>
+            </div>
+          );
+        })}
     </>
   );
 };
