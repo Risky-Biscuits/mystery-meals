7838ac1d33ba159cca76d58ba8ab2a0db1de344e
dthomas91 <davidthomas8891@gmail.com>
2022-11-15 10:24:07 -0800

    added additional test

--- app/javascript/components/jest-tests/RestaurantShow.test.js
+++ app/javascript/components/jest-tests/RestaurantShow.test.js
@@ -1,16 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
 import RestaurantShow from "../pages/RestaurantShow";
 import mockRestaurants from "../mockRestaurants"
+import restaurants from "../mockRestaurants";
 
 describe("<RestaurantShow />", () => {
   it("renders without crashing", () => {
     render(
-      <BrowserRouter>
-        <RestaurantShow restaurants={mockRestaurants}/>
-      </BrowserRouter>
+      <MemoryRouter initialEntries={["/restaurantshow/1"]}>
+        <Routes>
+          <Route path="/restaurantshow/:id" element={<RestaurantShow restaurants={mockRestaurants} />} />
+        </Routes>
+      </MemoryRouter>
     )
+    const showRender = screen.getByText(/Furry Tacos/i)
+    screen.debug(showRender)
     expect(screen.getByText("Restaurant Details")).toBeInTheDocument()
   })
 });
