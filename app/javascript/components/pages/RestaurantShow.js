7cf77c33a4dbb6302fd170cba05c44f9040e8640
dthomas91 <davidthomas8891@gmail.com>
2022-11-15 10:56:12 -0800

    refactored code for conditional rendering

--- app/javascript/components/pages/RestaurantShow.js
+++ app/javascript/components/pages/RestaurantShow.js
@@ -7,23 +7,28 @@ import Typography from "@mui/material/Typography";
 import React from "react";
 import { useParams } from "react-router-dom";
 
-
 const RestaurantShow = ({ restaurants }) => {
-  const { id } = useParams()
-  const currentRestaurant = restaurants?.find((restaurant) => restaurant.id === +id)
+  const { id } = useParams();
+  const currentRestaurant = restaurants?.find(
+    (restaurant) => restaurant.id === +id
+  );
   return (
     <>
       <h1>Restaurant Details</h1>
-      <h3>{currentRestaurant?.name}</h3>
-      <img src={currentRestaurant?.image} />
-      <p>{currentRestaurant?.food_type}</p>
-      <p>{currentRestaurant?.price}</p>
-      <p>{currentRestaurant?.phone_number}</p>
-      <p>{currentRestaurant?.website}</p>
-      <p>{currentRestaurant?.street}</p>
-      <p>{currentRestaurant?.city}</p>
-      <p>{currentRestaurant?.state}</p>
-      <p>{currentRestaurant?.zip}</p>
+      {currentRestaurant && (
+        <>
+          <h3>{currentRestaurant.name}</h3>
+          <img src={currentRestaurant.image} />
+          <p>{currentRestaurant.food_type}</p>
+          <p>{currentRestaurant.price}</p>
+          <p>{currentRestaurant.phone_number}</p>
+          <p>{currentRestaurant.website}</p>
+          <p>{currentRestaurant.street}</p>
+          <p>{currentRestaurant.city}</p>
+          <p>{currentRestaurant.state}</p>
+          <p>{currentRestaurant.zip}</p>
+        </>
+      )}
     </>
   );
 };
