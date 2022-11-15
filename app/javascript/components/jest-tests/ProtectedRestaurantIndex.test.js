72243d365016b5b50c51ecf285b446f1aa09060c
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-15 13:12:04 -0500

    added test for when logged in

--- app/javascript/components/jest-tests/ProtectedRestaurantIndex.test.js
+++ app/javascript/components/jest-tests/ProtectedRestaurantIndex.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import MockUser from "../jest-tests/__mockUser__/MockUser";
+import mockRestaurants from "../mockRestaurants";
 import ProtectedRestaurantIndex from "../pages/ProtectedRestaurantIndex";
 
 describe("<RestaurantIndex />", () => {
@@ -8,6 +10,14 @@ describe("<RestaurantIndex />", () => {
     render(
       <BrowserRouter>
         <ProtectedRestaurantIndex />
+      </BrowserRouter>
+    );
+  });
+
+  it("renders without crashing", () => {
+    render(
+      <BrowserRouter>
+        <ProtectedRestaurantIndex logged_in={true} current_user={MockUser} restaurants={mockRestaurants} />
       </BrowserRouter>
     );
   });
