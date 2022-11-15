98c1db155f3a26fad926f3dca676719faa2b7808
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-15 13:11:51 -0500

    added mock_user for test

---
+++ app/javascript/components/jest-tests/__mockUser__/MockUser.js
@@ -0,0 +1,8 @@
+const current_user = {
+  id: 1,
+  email: "test@example.com",
+  created_at: "2022-11-13T22:38:02.480Z",
+  updated_at: "2022-11-13T22:38:02.480Z",
+  first_name: "Ricky",
+  last_name: "Biscuits",
+};
