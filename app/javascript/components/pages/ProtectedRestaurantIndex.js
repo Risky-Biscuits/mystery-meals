98de6d8496fa1e6a9430ed77289dec0038cfe709
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-15 19:27:35 -0500

    updated logged_in

--- app/javascript/components/pages/ProtectedRestaurantIndex.js
+++ app/javascript/components/pages/ProtectedRestaurantIndex.js
@@ -15,54 +15,52 @@ const ProtectedRestaurantIndex = ({
   return (
     <div className="page-container">
       <h1>Protected Restaurant Index</h1>
-      {logged_in === true && (
+      {logged_in && (
         <>
-  
-            <h1>See All Restaurants</h1>
-            <div className="card-container">
-              {reviews
-                .filter((review) => review.user_id === current_user.id)
-                .map((review) => {
-                  return restaurants
-                    .filter(
-                      (restaurant) => restaurant.id === review.restaurant_id
-                    )
-                    .map((restaurant, index) => {
-                      return (
-                        <div key={index}>
-                          <Card
-                            elevation={24}
-                            sx={{ maxWidth: 300, border: "5px solid #AD8350" }}
-                          >
-                            <CardMedia
-                              component="img"
-                              height="340"
-                              image={restaurant.image}
-                              alt="restaurant"
-                            />
-                            <CardContent>
-                              <Typography
-                                gutterBottom
-                                variant="h5"
-                                component="div"
-                              >
-                                {restaurant.name}
-                              </Typography>
-                            </CardContent>
-                            <CardActions>
-                              <Button size="small">Details</Button>
-                              <Button size="small">Reviews</Button>
-                            </CardActions>
-                          </Card>
-                        </div>
-                      );
-                    });
-                })}
-            </div>
-         
+          <h1>See All Restaurants</h1>
+          <div className="card-container">
+            {reviews
+              .filter((review) => review.user_id === current_user.id)
+              .map((review) => {
+                return restaurants
+                  .filter(
+                    (restaurant) => restaurant.id === review.restaurant_id
+                  )
+                  .map((restaurant, index) => {
+                    return (
+                      <div key={index}>
+                        <Card
+                          elevation={24}
+                          sx={{ maxWidth: 300, border: "5px solid #AD8350" }}
+                        >
+                          <CardMedia
+                            component="img"
+                            height="340"
+                            image={restaurant.image}
+                            alt="restaurant"
+                          />
+                          <CardContent>
+                            <Typography
+                              gutterBottom
+                              variant="h5"
+                              component="div"
+                            >
+                              {restaurant.name}
+                            </Typography>
+                          </CardContent>
+                          <CardActions>
+                            <Button size="small">Details</Button>
+                            <Button size="small">Reviews</Button>
+                          </CardActions>
+                        </Card>
+                      </div>
+                    );
+                  });
+              })}
+          </div>
         </>
       )}
-      {logged_in === false && <>YOU MUST BE LOGGED IN TO SEE THIS PAGE</>}
+      {!logged_in && <>YOU MUST BE LOGGED IN TO SEE THIS PAGE</>}
     </div>
   );
 };
