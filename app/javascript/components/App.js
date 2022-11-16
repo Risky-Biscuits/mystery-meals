adb4cfcd78f19e4c5e8b21fe4ff30ead084d4dee
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-16 13:20:09 -0500

    added useEffect and useState for create

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -16,18 +16,48 @@ import RestaurantReviewNew from "./pages/RestaurantReviewNew";
 import RestaurantReviewShow from "./pages/RestaurantReviewShow";
 import RestaurantShow from "./pages/RestaurantShow";
 
-
 const App = (props) => {
+  const [restaurants, setRestaurants] = useState([]);
+  
+  useEffect(() => {
+    readRestaurant();
+  }, []);
+
+  const readRestaurant = () => {
+    fetch("http://localhost:3000/restaurants")
+      .then((response) => response.json())
+      .then((payload) => {
+        setRestaurants(payload);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const createRestaurant = (restaurant) => {
+    fetch("http://localhost:3000/restaurants", {
+      // converts the object to a string that can be passed in the request
+      body: JSON.stringify(restaurant),
+      // specify the info being sent in JSON and the info returning should be JSON
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // HTTP verb so the correct endpoint is invoked on the server
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then(() => readRestaurant())
+      .catch((errors) => console.log("Restaurant create errors:", errors));
+  };
+
   return (
     <BrowserRouter>
       <Header {...props} />
       <Routes>
         <Route exact path="/" element={<Home {...props} />} />
         {/* Restaurant Routes */}
-        <Route path="/restaurantindex" element={<RestaurantIndex {...props} restaurants={mockRestaurants} />} />
+        <Route path="/restaurantindex" element={<RestaurantIndex {...props} restaurants={restaurants} />} />
         <Route path="/protectedrestaurantindex" element={<ProtectedRestaurantIndex {...props} restaurants={mockRestaurants} reviews={mockReviews}/>} />
         <Route path="/restaurantshow/:id" element={<RestaurantShow {...props} restaurants={mockRestaurants}/>} />
-        <Route path="/restaurantnew" element={<RestaurantNew />} />
+        <Route path="/restaurantnew" element={<RestaurantNew createRestaurant={createRestaurant} />} />
         <Route path="/restaurantedit/:id" element={<RestaurantEdit />} />
         {/* Restaurant Review Routes */}
         <Route path="/restaurantreviewindex" element={<RestaurantReviewIndex />} />
