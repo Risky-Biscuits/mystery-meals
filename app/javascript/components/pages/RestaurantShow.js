ed3163db28b9df7134caf52e59fdfb404a3bbb50
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-17 22:48:04 -0500

    enhanced styling, added icons

--- app/javascript/components/pages/RestaurantShow.js
+++ app/javascript/components/pages/RestaurantShow.js
@@ -1,4 +1,4 @@
-import { Restaurant } from "@mui/icons-material";
+import { Phone, Restaurant, Savings } from "@mui/icons-material";
 import {
   Button,
   Card,
@@ -23,93 +23,117 @@ const RestaurantShow = ({ restaurants, logged_in }) => {
       {currentRestaurant && (
         <Card
           elevation={24}
-          sx={{ width: 900, border: "5px solid #AD8350", mb: "1rem" }}
+          sx={{ width: 650, border: "5px solid #AD8350", mb: "1rem" }}
         >
           <CardMedia
             component="img"
-            height="500px"
+            height="400px"
             image={currentRestaurant.image}
             alt="restaurant"
           />
-          <CardContent>
+          <div className="show-card-container">
             <CardContent>
-              <Typography
-                sx={{ fontWeight: "bold" }}
-                gutterBottom
-                variant="h4"
-                component="div"
-              >
-                <Restaurant /> {currentRestaurant.name}
-              </Typography>
-              <Typography
-                sx={{ fontSize: "1.2rem" }}
-                gutterBottom
-                variant="h7"
-                component="div"
-              >
-                {currentRestaurant.food_type}
-              </Typography>
-              <Typography
-                sx={{ fontSize: "1.2rem" }}
-                gutterBottom
-                variant="h7"
-                component="div"
-              >
-                Price-Range: {currentRestaurant.price}
-              </Typography>
-              <Typography
-                sx={{ fontSize: "1.2rem" }}
-                gutterBottom
-                variant="h7"
-                component="div"
-              >
-                Phone: {currentRestaurant.phone_number}
-              </Typography>
-              <Typography
-                sx={{ fontSize: "1.2rem" }}
-                gutterBottom
-                variant="h7"
-                component="div"
-              >
-                Address: {currentRestaurant.street}
-              </Typography>
-              <Typography
-                sx={{ fontSize: "1.2rem" }}
-                gutterBottom
-                variant="h7"
-                component="div"
-              >
-                City: {currentRestaurant.city}
-              </Typography>
-              <Typography
-                sx={{ fontSize: "1.2rem" }}
-                gutterBottom
-                variant="h7"
-                component="div"
-              >
-                State: {currentRestaurant.state}
-              </Typography>
-              <Typography
-                sx={{ fontSize: "1.2rem" }}
-                gutterBottom
-                variant="h7"
-                component="div"
-              >
-                Zip Code: {currentRestaurant.zip}
-              </Typography>
-              <Typography
-                sx={{ fontSize: "1.2rem" }}
-                gutterBottom
-                variant="h7"
-                component="div"
-              >
-                <a href={`${currentRestaurant.website}`}>
-                  Visit Their Website{" "}
-                </a>
-              </Typography>
+              <CardContent>
+                <Typography
+                  sx={{ fontWeight: "bold" }}
+                  gutterBottom
+                  variant="h4"
+                  component="div"
+                >
+                  <Restaurant /> {currentRestaurant.name}
+                </Typography>
+                <div className="show-card-container">
+                  <div className="show-card-info">
+                    <Typography
+                      sx={{ fontSize: "1.4rem", fontWeight: "bold" }}
+                      gutterBottom
+                      variant="h7"
+                      component="div"
+                    >
+                      {currentRestaurant.food_type}
+                    </Typography>
+                    <Typography
+                      sx={{ fontSize: "1.2rem" }}
+                      gutterBottom
+                      variant="h7"
+                      component="div"
+                    >
+                      <Savings sx={{ verticalAlign: "bottom" }} />{" "}
+                      {currentRestaurant.price}
+                    </Typography>
+                    <Typography
+                      sx={{ fontSize: "1.2rem" }}
+                      gutterBottom
+                      variant="h7"
+                      component="div"
+                    >
+                      <Phone sx={{ verticalAlign: "bottom" }} />{" "}
+                      {currentRestaurant.phone_number}
+                    </Typography>
+
+                    <div className="card-address-container">
+                      <Typography
+                        sx={{ fontSize: "1.2rem", fontWeight: "bold" }}
+                        gutterBottom
+                        variant="h7"
+                        component="div"
+                      >
+                        Address:
+                      </Typography>
+
+                      <Typography
+                        sx={{ fontSize: "1.2rem" }}
+                        gutterBottom
+                        variant="h7"
+                        component="div"
+                      >
+                        {currentRestaurant.street}
+                      </Typography>
+                      <div className="horizontal-flex-container">
+                        <Typography
+                          sx={{ fontSize: "1.2rem" }}
+                          gutterBottom
+                          variant="h7"
+                          component="div"
+                        >
+                          {currentRestaurant.city},
+                        </Typography>
+                        &nbsp;
+                        <Typography
+                          sx={{ fontSize: "1.2rem" }}
+                          gutterBottom
+                          variant="h7"
+                          component="div"
+                        >
+                          {currentRestaurant.state}
+                        </Typography>
+                        &nbsp;
+                        <Typography
+                          sx={{ fontSize: "1.2rem" }}
+                          gutterBottom
+                          variant="h7"
+                          component="div"
+                        >
+                          {currentRestaurant.zip}
+                        </Typography>
+                      </div>
+                    </div>
+
+                    <Typography
+                      sx={{ fontSize: "1.2rem" }}
+                      gutterBottom
+                      variant="h7"
+                      component="div"
+                    >
+                      <a href={`${currentRestaurant.website}`}>
+                        Visit Their Website{" "}
+                      </a>
+                    </Typography>
+                  </div>
+                </div>
+              </CardContent>
             </CardContent>
-          </CardContent>
-          <CardActions></CardActions>
+          </div>
         </Card>
       )}
       {/* ------------------------------ ⬇️ When Logged Out ⬇️ ------------------------------------- */}
