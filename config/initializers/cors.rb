a97903e96e660f542a7c82dd4291fba076a65f4f
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-21 18:18:06 -0500

    added cors support

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://risky-biscuits.herokuapp.com/'  # <- change this to allow requests from any domain while in development.
+    origins '*'  # <- change this to allow requests from any domain while in development.
 
     resource '*',
       headers: :any,
