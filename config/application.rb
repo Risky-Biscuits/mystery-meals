06983ece09bce727eedc9bf36a672dd3983f869b
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-21 14:29:11 -0500

    assest compiling line

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module RiskyBiscuitApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
   end
 end
